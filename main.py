d64c9f5389439ea956ad4a1ca2223e7245e0923c
SergeiK1 <sergeikdr1@gmail.com>
2023-09-13 20:24:40 -0400

    trading strategt

--- main.py
+++ main.py
@@ -3,30 +3,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import yfinance as yf
+import time
+import pandas as pd
 
-def fetch_data(ticker_symbol):
-    # Create a Ticker object
-    ticker = yf.Ticker(ticker_symbol)
-
-    # Fetch historical data
-    hist_data = ticker.history(period="1d")  # Fetches data for the latest day. You can adjust the period as needed.
-
-    # Fetch other details if required
-    info = ticker.info
-
-    return hist_data, info
-
-# Example usage:
-ticker_symbol = "AAPL"  # Replace with your desired ticker symbol
-data, info = fetch_data(ticker_symbol)
-print(data)
-print(info)
 
 
 
 # GLOBAL VARIBLES 
 acc_username = "NJ_21_ZZ1125"
 acc_password = "EXCZ4300" 
+total_capital = 100000
 
 
 def buy_stock(driver, ticker, number_of_shares):
@@ -290,6 +276,134 @@ def trade_stocks(action, ticker, number_of_shares):
 
     # Example usage:
     # trade_stocks('buy', 'AAPL', 5, 'your_username', 'your_password')
+
+
+
+# -------------------------------------------   TRADING STRATEGY   ---------------------------------------------
+
+# List of stocks to monitor
+stocks_to_monitor = ["META", "AAPL", "AMZN", "CVNA", "PTON", "TSLA", "UPST", "BROS", "C", "DIS"]
+
+def fetch_data(ticker_symbol):
+    # Create a Ticker object
+    ticker = yf.Ticker(ticker_symbol)
+    # Fetch historical data for 15-minute intervals
+    hist_data = ticker.history(period="1d", interval="15m")
+    return hist_data
+
+def calculate_shares_to_buy(entry_price, stop_loss_price, total_capital, risk_percentage=0.01):
+    risk_per_trade = total_capital * risk_percentage
+    risk_per_share = abs(entry_price - stop_loss_price)
+    
+    # Calculate the number of shares
+    num_shares = int(risk_per_trade / risk_per_share)
+    
+    # Ensure you don't spend more than your capital
+    total_cost = num_shares * entry_price
+    if total_cost > total_capital:
+        num_shares = int(total_capital / entry_price)
+    
+    return num_shares
+
+def trade_strategy(ticker_symbol):
+    data = fetch_data(ticker_symbol)
+    
+    # Calculate MACD and Signal line
+    data['EMA12'] = data['Close'].ewm(span=12, adjust=False).mean()
+    data['EMA26'] = data['Close'].ewm(span=26, adjust=False).mean()
+    data['MACD'] = data['EMA12'] - data['EMA26']
+    data['Signal_Line'] = data['MACD'].ewm(span=9, adjust=False).mean()
+    
+    # Calculate RSI
+    delta = data['Close'].diff()
+    gain = (delta.where(delta > 0, 0)).fillna(0)
+    loss = (-delta.where(delta < 0, 0)).fillna(0)
+    avg_gain = gain.rolling(window=14).mean()
+    avg_loss = loss.rolling(window=14).mean()
+    rs = avg_gain / avg_loss
+    data['RSI'] = 100 - (100 / (1 + rs))
+    
+    # Buy and Sell signals
+    buy_signals = []
+    sell_signals = []
+    
+    for i in range(1, len(data)):
+        # Buy Entry
+        if data['MACD'].iloc[i] > data['Signal_Line'].iloc[i] and data['RSI'].iloc[i] > 30:
+            buy_signals.append(data['Close'].iloc[i])
+            sell_signals.append(float('nan'))
+        # Sell Entry
+        elif data['MACD'].iloc[i] < data['Signal_Line'].iloc[i] and data['RSI'].iloc[i] < 70:
+            sell_signals.append(data['Close'].iloc[i])
+            buy_signals.append(float('nan'))
+        else:
+            buy_signals.append(float('nan'))
+            sell_signals.append(float('nan'))
+    
+    data['Buy_Signal'] = buy_signals
+    data['Sell_Signal'] = sell_signals
+    
+     # Execute trades based on signals
+    for i in range(1, len(data)):
+        if not pd.isna(data['Buy_Signal'].iloc[i]):
+            # Calculate the number of shares to buy
+            shares_to_buy = calculate_shares_to_buy(data['Close'].iloc[i], data['Close'].iloc[i-1], total_capital)
+            trade_stocks('buy', ticker_symbol, shares_to_buy)
+        elif not pd.isna(data['Sell_Signal'].iloc[i]):
+            # Calculate the number of shares to sell
+            shares_to_sell = calculate_shares_to_buy(data['Close'].iloc[i], data['Close'].iloc[i-1], total_capital)
+            trade_stocks('sell', ticker_symbol, shares_to_sell)
+
+
+
+
+# --------------------------  Execute Code  -------------------------------
+
+
+
+# Monitor the stocks
+while True:
+    for stock in stocks_to_monitor:
+        trade_strategy(stock)
+    time.sleep(300) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Day Trading Strategy Using RSI and MACD:
